c13e7365a7b4251fdb31b0989e061c8e3130621a
Daniel Cramon <danielcramon@gmail.com>
2024-12-18 10:00:43 +0100

    scene updated and spawn system scheduling enabled

--- Assets/Scripts/Systems/CatapultSpawnerSystem.cs
+++ Assets/Scripts/Systems/CatapultSpawnerSystem.cs
@@ -38,11 +38,70 @@ partial struct CatapultSpawnerSystem : ISystem
                 ECB.AddComponent(e, LocalTransform.FromPosition(spawner.catapultOffset + new float3(x, y, z)));
             }
         }
-    }
 
-    [BurstCompile]
-    public void OnDestroy(ref SystemState state)
+        if (data.schedulingType == SchedulingType.Schedule)
+        {
+            var ECB = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged);
+
+            state.Dependency = new spawnCatapults
+            {
+                ecb = ECB
+            }.Schedule(state.Dependency);
+        }
+
+        if (data.schedulingType == SchedulingType.ScheduleParallel)
+        {
+            var ECB = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+            state.Dependency = new spawnCatapultsParallel
+            {
+                ecb = ECB
+            }.ScheduleParallel(state.Dependency);
+        }
+    }
+}
+
+[BurstCompile]
+public partial struct spawnCatapults : IJobEntity
+{
+
+    public EntityCommandBuffer ecb;
+    public void Execute(in CatapultSpawnerComponent spawner)
     {
-        
+        int n = spawner.spawnAmmount;
+        int width = spawner.catapultRowWidth;
+        float y = 0f;
+
+        for (int i = 0; i < n; i++)
+        {
+            var e = ecb.Instantiate(spawner.catapultPrefab);
+            float x = (i % width) * 7.5f;
+            float z = -(i / width) * 15f;
+
+            ecb.AddComponent(e, LocalTransform.FromPosition(spawner.catapultOffset + new float3(x, y, z)));
+        }
+    }
+}
+
+[BurstCompile]
+public partial struct spawnCatapultsParallel : IJobEntity
+{
+
+    public EntityCommandBuffer.ParallelWriter ecb;
+    public void Execute([ChunkIndexInQuery] int key, in CatapultSpawnerComponent spawner)
+    {
+        int n = spawner.spawnAmmount;
+        int width = spawner.catapultRowWidth;
+        float y = 0f;
+
+        for (int i = 0; i < n; i++)
+        {
+            var e = ecb.Instantiate(key, spawner.catapultPrefab);
+            float x = (i % width) * 7.5f;
+            float z = -(i / width) * 15f;
+
+            ecb.AddComponent(key, e, LocalTransform.FromPosition(spawner.catapultOffset + new float3(x, y, z)));
+        }
     }
 }
